7d1e5d1c02bfa8788947e13a31cab4abc9f1e273
kevin <kjhenner@gmail.com>
2024-07-24 20:49:59 -0700

    projection stuff fixed

--- tensor_beasts/display_manager.py
+++ tensor_beasts/display_manager.py
@@ -9,7 +9,7 @@ from OpenGL.GL import (
     glBindTexture, glClear, glTexCoord2f, glVertex2f, glBegin, glEnd,
     glTexImage2D, glTexParameteri, glTexSubImage2D, glEnable, glGenTextures,
     GL_TEXTURE_2D, GL_RGB, GL_UNSIGNED_BYTE, GL_LINEAR, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT, GL_QUADS,
-    GL_TEXTURE_MIN_FILTER, GL_TEXTURE_MAG_FILTER, glLoadIdentity, glScale, glTranslate,
+    GL_TEXTURE_MIN_FILTER, GL_TEXTURE_MAG_FILTER, glLoadIdentity,
     GL_NEAREST, glViewport, GL_PROJECTION, GL_MODELVIEW, glMatrixMode
 )
 from OpenGL import GLU
@@ -21,8 +21,6 @@ class DisplayManager:
         self.world_width = world_width
         self.world_height = world_height
 
-        print(f"World width: {world_width}")
-        print(f"World height: {world_height}")
         self.window_width = world_width
         self.window_height = world_height
 
@@ -43,8 +41,7 @@ class DisplayManager:
         # Initialize texture
         self.texture = glGenTextures(1)
         glBindTexture(GL_TEXTURE_2D, self.texture)
-        # self.screen = np.ones((self.world_width, self.world_height, 3), dtype=np.uint8) * 255
-        self.screen = np.arange(self.world_width * self.world_height * 3, dtype=np.uint8).reshape(self.world_width, self.world_height, 3)
+        self.screen = np.zeros((self.world_height, self.world_width, 3), dtype=np.uint8)
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, self.world_width, self.world_height, 0, GL_RGB, GL_UNSIGNED_BYTE, self.screen)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
@@ -55,7 +52,7 @@ class DisplayManager:
         self.last_update_time = time.time()
 
         if self.world_width < 256 and self.world_height < 256:
-            self.resize(256, 256)
+            self.resize(int(256 * self.world_aspect), 256)
 
     def update(self):
         current_time = time.time()
@@ -65,16 +62,11 @@ class DisplayManager:
             self.last_update_time = current_time
 
         if self.dirty:
-            print("UPDATE")
             screen = np.flipud(self.screen)
-            screen = self.screen
-            print(f"Screen shape: {screen.shape}")
             glBindTexture(GL_TEXTURE_2D, self.texture)
             glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, self.world_width, self.world_height, GL_RGB, GL_UNSIGNED_BYTE, screen)
 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-
-            self.update_projection()
 
             glBegin(GL_QUADS)
             glTexCoord2f(0, 0); glVertex2f(-self.world_aspect, -1)
@@ -83,12 +75,7 @@ class DisplayManager:
             glTexCoord2f(0, 1); glVertex2f(-self.world_aspect, 1)
             glEnd()
 
-            # glBegin(GL_QUADS)
-            # glTexCoord2f(0, 0); glVertex2f(-1, -1)
-            # glTexCoord2f(0, 1); glVertex2f(1, -1)
-            # glTexCoord2f(1, 1); glVertex2f(1, 1)
-            # glTexCoord2f(1, 0); glVertex2f(-1, 1)
-            # glEnd()
+            self.update_projection()
 
             pygame.display.flip()
             self.dirty = False
@@ -101,40 +88,10 @@ class DisplayManager:
 
     def update_screen(self, screen: torch.Tensor):
         self.dirty = True
-        print(f"Input tensor shape: {screen.shape}")  # Expected: (8, 32)
-        print(f"Input corner values:")
-        print(f"Top-left: {screen[0, 0]:.0f}")
-        print(f"Top-right: {screen[0, -1]:.0f}")
-        print(f"Bottom-left: {screen[-1, 0]:.0f}")
-        print(f"Bottom-right: {screen[-1, -1]:.0f}")
-        assert screen.shape == (8, 32)
-        assert screen[0, 0] == 0
-        assert screen[0, -1] == 31
-        assert screen[-1, 0] == 224
-        assert screen[-1, -1] == 255
         if screen.size() == (self.world_height, self.world_width):
             # Tensors are in shape H, W
             screen = screen.unsqueeze(-1).expand(-1, -1, 3)
-
-        screen = screen.permute(1, 0, 2)
-        assert screen.shape == (32, 8, 3)  # Expected: (32, 16, 3) (W, H, C)
-        assert screen[0, 0, 0] == 0  # top left
-        assert screen[0, -1, 0] == 224  # bottom left
-        assert screen[-1, 0, 0] == 31  # top right
-        assert screen[-1, -1, 0] == 255  # bottom right
-
-        # Pygame expects the screen to be in shape W, H, C
-        # Tensors are in shape H, W, C
-        # We keep the screen right-side up for now so we can index into it easily,
-        # but we will flip it upside down when we render it because OpenGL has the
-        # origin at the bottom left.
         self.screen[:] = screen.cpu().numpy()
-
-        assert self.screen.shape == (32, 8, 3)  # Expected: (32, 16, 3) (W, H, C)
-        assert self.screen[0, 0, 0] == 0  # top left
-        assert self.screen[0, -1, 0] == 224  # bottom left
-        assert self.screen[-1, 0, 0] == 31  # top right
-        assert self.screen[-1, -1, 0] == 255  # bottom right
 
     def add_screens_to_buffer(self, screens: torch.Tensor):
         current_time = time.time()
@@ -190,7 +147,6 @@ class DisplayManager:
 
         glMatrixMode(GL_MODELVIEW)
 
-
     def resize(self, width, height):
         self.window_width = width
         self.window_height = height
@@ -216,5 +172,5 @@ class DisplayManager:
         grid_x = max(0, min(grid_x, self.world_width - 1))
         grid_y = max(0, min(grid_y, self.world_height - 1))
 
-        print(f"Screen value: {self.screen[grid_x][grid_y]}")
+        print(f"Screen value: {self.screen[grid_y][grid_x]}")
         return grid_x, grid_y
